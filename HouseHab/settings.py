437b254a550a7eba61e67a0c4db5cc102bd390f2
tamirlan1919 <tchinchaev@bk.ru>
2024-06-04 19:33:27 +0300

    Закончил API 2.1

--- HouseHab/settings.py
+++ HouseHab/settings.py
@@ -32,7 +32,6 @@ CSRF_TRUSTED_ORIGINS = ['https://tamirlan1919-househab-8a3f.twc1.net']
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:5173',
 # ]
-CORS_ORIGIN_ALLOW_ALL = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
@@ -52,14 +51,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'drf_spectacular',
-    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
